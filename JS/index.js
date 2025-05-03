96617027421f828965affa46910cd1a019316315
Seth <samaritanbrotherseth@gmail.com>
2025-05-03 17:50:56 -0400

    completed footer element

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,15 @@
-const footer = document.createElement("footer");
+const newDate = new Date();
+const thisYear = newDate.getFullYear();
+
+//access the footer by class
+const footer = document.querySelector(".footer");
+
+//create a new paragraph for the copyright
+const copyright = document.createElement("p");
+copyright.setAttribute("id", "copyright");
+copyright.innerText = "\u00A9 Seth Johnson " + thisYear;
+
+//append the copyright item to the footer
+footer.appendChild(copyright);
+
+copyright.style.fontFamily = "Times New Roman";
