3678c804412dd502332c12b94fe43a43e42ddd2f
Seth <samaritanbrotherseth@gmail.com>
2025-05-03 19:05:26 -0400

    js added, created footer and skills

--- JS/index.js
+++ JS/index.js
@@ -13,3 +13,15 @@ copyright.innerText = "\u00A9 Seth Johnson " + thisYear;
 footer.appendChild(copyright);
 
 copyright.style.fontFamily = "Times New Roman";
+
+const skills = ["Github", "HTML", "CSS", "Python"]; //array of skills
+const skillsection = document.getElementById("skills"); //find the element ul created
+
+const skillsList = skillsection.getElementsByTagName("ul")[0]; //using the first ul in the document
+for (let i = 0; i < skills.length; i++) {
+  //direction wanted bracket not forEach method
+  //address each element for it self and adds to list
+  let skill = document.createElement("li"); //create li elements
+  skill.innerText = skills[i]; // for ever skill there will be a li
+  skillsList.appendChild(skill); //make child to the ul created earlier
+}
